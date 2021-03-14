de838e79a9238dd50e3a686f897cba1ace85e048
jfortuny <jfortuny@jfortuny.com>
2021-03-14 14:16:09 -0400

    Added tryCatch to excel and farmland tab existence

--- code/ReadSpreadsheet.R
+++ code/ReadSpreadsheet.R
@@ -6,10 +6,28 @@ dataDir <- paste(baseDir, '/data', sep = '')
 filePath <- file.path(paste(dataDir, '/Crop Rotation Sample Template.xlsx', sep = ''))
 
 # List the sheets/tabs in the sample template spreadsheet
-sheets <- excel_sheets(filePath)
+tryCatch(sheets <- excel_sheets(filePath),
+         error = function(e){
+           message('The spreadsheet does not exist')
+           quit(save = 'no')
+         },
+         warning = function(w){
+           message(paste('Warning:', w))
+           quit(save = 'no')
+         }
+)
 
 # Read Farmland sheet; will throw error it the sheet doesn't exist
-farmland <- read_excel(filePath, sheet = 'Farmland')
+tryCatch(farmland <- read_excel(filePath, sheet = 'Farmland'),
+         error = function(e){
+           message('There is no Farmland tab in the spreadsheet')
+           quit(save = 'no')
+         },
+         warning = function(w){
+           message(paste('Warning:', w))
+           quit(save = 'no')
+         }
+         )
 # Keep only included fields
 farmlandIncluded <- subset(as.data.frame(farmland), farmland$'Include?' == 'Y')
 
