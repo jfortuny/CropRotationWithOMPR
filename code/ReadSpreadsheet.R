19e88e352d5e99683a7388cebfafb994467aeff9
jfortuny <jfortuny@jfortuny.com>
2021-03-14 16:15:44 -0400

    Added more text to the documentation file

--- code/ReadSpreadsheet.R
+++ code/ReadSpreadsheet.R
@@ -6,28 +6,11 @@ dataDir <- paste(baseDir, '/data', sep = '')
 filePath <- file.path(paste(dataDir, '/Crop Rotation Sample Template.xlsx', sep = ''))
 
 # List the sheets/tabs in the sample template spreadsheet
-tryCatch(sheets <- excel_sheets(filePath),
-         error = function(e){
-           message('The spreadsheet does not exist')
-           quit(save = 'no')
-         },
-         warning = function(w){
-           message(paste('Warning:', w))
-           quit(save = 'no')
-         }
-)
+sheets <- excel_sheets(filePath)
 
 # Read Farmland sheet; will throw error it the sheet doesn't exist
-tryCatch(farmland <- read_excel(filePath, sheet = 'Farmland'),
-         error = function(e){
-           message('There is no Farmland tab in the spreadsheet')
-           quit(save = 'no')
-         },
-         warning = function(w){
-           message(paste('Warning:', w))
-           quit(save = 'no')
-         }
-         )
+farmland <- read_excel(filePath, sheet = 'Farmland')
+         
 # Keep only included fields
 farmlandIncluded <- subset(as.data.frame(farmland), farmland$'Include?' == 'Y')
 
