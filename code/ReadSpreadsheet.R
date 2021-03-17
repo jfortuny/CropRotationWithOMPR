ddd154cc976b741fc996300072811f81ceb5f4e2
jfortuny <jfortuny@jfortuny.com>
2021-03-17 10:27:46 -0400

    Processing the spreadsheet to produce relevant variables.

--- code/ReadSpreadsheet.R
+++ code/ReadSpreadsheet.R
@@ -1,4 +1,6 @@
 library (readxl)
+library(tidyr)
+library(dplyr)
 
 # Define path to sample template spreadsheet
 baseDir <- getwd()
@@ -12,5 +14,35 @@ sheets <- excel_sheets(filePath)
 farmland <- read_excel(filePath, sheet = 'Farmland')
          
 # Keep only included fields
-farmlandIncluded <- subset(as.data.frame(farmland), farmland$'Include?' == 'Y')
+fieldsIncluded <- subset(as.data.frame(farmland), farmland$'Include?' == 'Y')
+
+# Read demand
+demand <- read_excel(filePath, sheet = 'Demand')
+
+# Read crops (skip first row which only contains the label Planting Dates)
+crops <- read_excel(filePath, sheet = 'Crops', skip = 1)
+
+# Keep only selected fields
+cropsIncluded <- subset(as.data.frame(crops), crops$'Plant?' == 'Y')
+
+# To construct model variables we'll use the following naming convention:
+# Suffixes: c = crop; f = field, y = year, m = month
+c <- cropsIncluded[,'Crop']
+f <- fieldsIncluded[,'Field']
+y <- seq(1:4)
+m <- month.abb
+
+# Crops planted variables: a crop can be planted on a field in a year and a month; we need to create
+# the binary variables that represent possible planting dates
+# Start by gathering the crops included and their planting months
+cropPlantingMonths <- cropsIncluded %>% 
+  select('Crop', 'Jan':'Dec') %>%
+  gather(PlantingMonth, CanPlant, 'Jan':'Dec') %>%
+  filter(CanPlant == 'Y')
+# Identify the month from planting when the crop will release the field: this is the month after the harvest
+cropFieldOccupation <- cropsIncluded %>%
+  select('Crop', 'Days To Maturity')
+cropFieldRelease <- cropFieldOccupation %>% 
+  mutate('Months In Field' = round(`Days To Maturity`/30)) %>%
+  select('Crop', 'Months In Field')
 
