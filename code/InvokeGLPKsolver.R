083f053d2381eef0508390c9b26981e02a9fd6b8
jfortuny <jfortuny@jfortuny.com>
2021-04-15 07:45:23 -0400

    continued work on building the cplex data file

--- code/InvokeGLPKsolver.R
+++ code/InvokeGLPKsolver.R
@@ -1,15 +1,50 @@
 library(dplyr)
+# library(fs)
+
 # In this script we create the data in the CPLEX LP format, we store the data in a file for review
 # via lpsolve and visually and then invoke the solver (after reading the lp file again - a bit kludgy)
 
+# CPLEX LP File Storage location
+baseDir <- getwd()
+dataDir <- paste(baseDir, '/data/cplexData', sep = '')
+fileName <- paste('model-', as.character(Sys.Date()), sep = '')
+filePath <- file.path(paste(dataDir, '/', fileName, '.lp', sep = ''))
+
 # Create the stub for the problem sections
-title = c('\\* Crop Rotation', as.character(lubridate::year(Sys.Date())), '*\\')
-objective = 'minimize \nZ: '  # use cat to print the newline character
+title = paste('\\* Crop Rotation ', as.character(lubridate::year(Sys.Date())), ' *\\', sep = '')
+objective = c('', 'minimize', 'Z:')
 constraintsLand = ''
 constraintsDemand = ''
 bounds = ''
-general = ''
-integer = ''
-binary = ''
+general = c('', 'general')
+integer = c('', 'integer')
+binary = c('', 'binary')
 
-cat(title)
+# BUILD VARIABLES
+# Add Variables related to Crops Planted: varCropsPlantingMonthYearFieldFull$varID
+binary = c(binary, paste(' ', varCropsPlantingMonthYearFieldFull$varID, sep = ''))
+
+# Add variables related to fields not planted: varUnusedFieldYearMonth$varID
+binary = c(binary, varUnusedFieldYearMonth$varID)
+
+# Add variables related to unmet demand: varUnmetDemandProductYear$varID
+general <- c(general, varUnmetDemandProductYear$varID)
+
+# BUILD OBJECTIVE FUNCTION
+row <- paste(' + ', as.character(penaltyForUnmetDemand), ' ', varUnmetDemandProductYear$varID, sep = '')
+objective <- c(objective, row)
+
+# BUILD CONSTRAINTS
+
+# WRITE TO FILE
+# Write the title with overwrite
+write(title, file = filePath, ncolumns = 1, append = FALSE)
+
+# Append the Objective function
+write(objective, file = filePath, ncolumns = 1, append = TRUE)
+
+# Append Binary Variables
+write(binary, file = filePath, ncolumns = 1, append = TRUE)
+
+# Append Continuous/general Variables
+write(general, file = filePath, ncolumns = 1, append = TRUE)
--- code/Main.R
+++ code/Main.R
@@ -10,11 +10,12 @@ filePath <- file.path(paste(codeDir, '/ReadSpreadsheet.R', sep = ''))
 source(filePath)
 
 # Prepare Model Variables
-filePath <- file.path(paste(codeDir, '/PrepareModel.R', sep = ''))
+filePath <- file.path(paste(codeDir, '/PrepareModelData.R', sep = ''))
 source(filePath)
 
 # Set hard coded parameters
 penaltyForUnmetDemand <- 100
+valueOfCoverCrop <- penaltyForUnmetDemand/5
 
 # Invoke a solver
 
